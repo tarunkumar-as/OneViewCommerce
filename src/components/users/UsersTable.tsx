6a50d03253a1cd834e66cb42c3762481cbacf3dc
tarunkumar-as <tarunkumar.as@outlook.com>
2021-04-12 13:30:22 -0400

    Fixing console error

--- src/components/users/UsersTable.tsx
+++ src/components/users/UsersTable.tsx
@@ -62,7 +62,7 @@ export default function UsersTable(props: UsersTableProps) {
                 </TableRow>
               );
             } else {
-              return <div></div>;
+              return null;
             }
           })}
         </TableBody>
